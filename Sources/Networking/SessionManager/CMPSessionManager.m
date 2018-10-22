d2e7d33dc7bf6419a4ed70ce5db6d86ce04c8145
Dominik Kowalski <d.kowalsky92@gmail.com>
2018-10-22 12:47:37 +0200

    added license to project files

--- Sources/Networking/SessionManager/CMPSessionManager.m
+++ Sources/Networking/SessionManager/CMPSessionManager.m
@@ -1,14 +1,26 @@
 //
-//  SessionManager.m
-//  CMPComapiFoundation
+// The MIT License (MIT)
+// Copyright (c) 2017 Comapi (trading name of Dynmark International Limited)
 //
-//  Created by Dominik Kowalski on 14/08/2018.
-//  Copyright © 2018 Comapi. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "CMPSessionManager.h"
 #import "CMPSession.h"
 #import "CMPServices.h"
+#import "CMPKeychain.h"
+#import "CMPLogger.h"
 
 NSString * const authTokenKeychainItemNamePrefix = @"ComapiSessionToken_";
 NSString * const sessionDetailsUserDefaultsPrefix = @"ComapiSessionDetails_";
@@ -93,7 +105,7 @@ NSString * const sessionDetailsUserDefaultsPrefix = @"ComapiSessionDetails_";
     }
 }
 
-// MARK: - CMPAuthChallengeHandler
+#pragma mark - CMPAuthChallengeHandler
 
 - (void)authenticationFailedWithError:(nonnull NSError *)error {
     [self.requestManager tokenUpdateFailed];
@@ -139,7 +151,7 @@ NSString * const sessionDetailsUserDefaultsPrefix = @"ComapiSessionDetails_";
     }];
 }
 
-// MARK: - CMPSessionAuthProvider
+#pragma mark - CMPSessionAuthProvider
 
 - (void)authenticateWithSuccess:(void(^)(void))success failure:(void(^)(NSError *))failure {
     if (self.client) {
