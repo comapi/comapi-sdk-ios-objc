1a0c1338f0ff20f00e8750ff4a3522201d5d6a31
Marcin Swierczek <marcin.swierczek@dotdigital.com>
2022-04-22 12:53:11 +0200

    bump version number

--- Sources/Core/Constants/CMPConstants.m
+++ Sources/Core/Constants/CMPConstants.m
@@ -49,7 +49,7 @@ CMPHTTPHeaderType const CMPHTTPHeaderAuthorization = @"Authorization";
 CMPHTTPHeaderType const CMPHTTPHeaderIfMatch = @"If-Match";
 
 CMPSDKInfo const CMPSDKInfoPlatform = @"iOS";
-CMPSDKInfo const CMPSDKInfoVersion = @"2.0.1";
+CMPSDKInfo const CMPSDKInfoVersion = @"2.1.0";
 CMPSDKInfo const CMPSDKInfoType = @"native";
 
 CMPQueueName const CMPQueueNameConsole = @"com.comapi.foundation.console";
